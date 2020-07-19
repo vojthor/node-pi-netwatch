9cc7355f476a68105af0d70a4bcfe3ff0d33f6bd
vojthor <vojtech@nezdara.dev>
2020-07-19 21:19:00 +0200

    init commit, the true one

--- index.js
+++ index.js
@@ -1,18 +1,77 @@
-const nmap = require('node-nmap');
- 
-nmap.nmapLocation = "nmap"; //default
- 
-//Accepts array or comma separated string of NMAP acceptable hosts
-const quickscan = new nmap.QuickScan('192.168.1.0/24'); 
-quickscan.startScan();
+const nmap = require("node-nmap");
+const JVSDisplayOTron = require("jvsdisplayotron");
 
-quickscan.on('complete', function(data){
-    //console.log(data);
-    data.forEach(host => {
-        console.log(`${host.ip} - ${host.hostname}`);
+const dothat = new JVSDisplayOTron.DOTHAT();
+dothat.barGraph.setBrightness(0, 15);
+dothat.lcd.setContrast(45);
+dothat.backlight.setToHue(0.6);
+dothat.lcd.write("Scanning...");
+
+nmap.nmapLocation = "nmap"; //default
+
+const displayTotalDevices = (total) => {
+  dothat.lcd.setCursorPosition(0, 2);
+  dothat.lcd.write(`Online: ${total}`);
+};
+
+const waitFor = (ms) => new Promise((r) => setTimeout(r, ms));
+
+async function asyncForEach(array, callback) {
+  for (let index = 0; index < array.length; index++) {
+    await callback(array[index], index, array);
+  }
+}
+
+const scanTheNetwork = async () => {
+  return new Promise((resolve) => {
+    console.log("new run");
+    const quickscan = new nmap.QuickScan("192.168.1.0/24");
+
+    let blinkTimeout;
+    let graphState = true;
+
+    function blinkBlink() {
+      blinkTimeout = setTimeout(() => {
+        console.log("blink blink");
+        dothat.barGraph.setByPercentage(graphState ? 100 : 0);
+        graphState = !graphState;
+        blinkBlink();
+      }, 1000);
+    }
+
+    // blinkBlink();
+
+    function cancelBlink() {
+      clearTimeout(blinkTimeout);
+    }
+
+    quickscan.on("complete", async (data) => {
+      displayTotalDevices(data.length);
+      await asyncForEach(data, async (host) => {
+        await waitFor(6000);
+        dothat.lcd.clear();
+        displayTotalDevices(data.length);
+        dothat.lcd.setCursorPosition(0, 0);
+        dothat.lcd.write(host.ip);
+        dothat.lcd.setCursorPosition(0, 1);
+        dothat.lcd.write(host.hostname ? host.hostname : " ");
+        console.log(host);
+      });
+
+      // cancelBlink();
+      scanTheNetwork();
+    });
+
+    quickscan.on("error", function (error) {
+      console.log(error);
     });
   });
-   
-  quickscan.on('error', function(error){
-    console.log(error);
-  });
+};
+
+async function runTheStuff() {
+  while (true) {
+    await scanTheNetwork();
+  }
+}
+
+runTheStuff();
